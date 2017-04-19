0e15bf42dae7f58a77344fc5d778cca683bc325c
Jon Love <jonlov4@gmail.com>
2017-04-19 10:47:11 -0400

    required pm2 files

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'personal-site',
+      script    : 'app.js',
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      key  : '~/.ssh/id_rsa',
+      user : 'root',
+      host : '162.243.197.96',
+      ref  : 'origin/master',
+      repo : 'https://github.com/jayvolr/personal-site.git',
+      path : '~/personal-site',
+      'post-deploy' : 'nvm install && npm install && /root/.nvm/versions/node/v7.9.0/bin/pm2 reload ecosystem.config.js --env production && pm2 startOrRestart ecosystem.config.js'
+    },
+  }
+};
